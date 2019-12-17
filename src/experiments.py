26b31314830aa7fb9e207d43faa72b6f6d151bfa
pedrolarben <pedrolarabenitez@gmail.com>
2019-12-17 13:27:31 +0100

    prepare experiments

--- src/experiments.py
+++ src/experiments.py
@@ -1,8 +1,8 @@
 import itertools
 import numpy as np
-import pandas as pd
+import tensorflow as tf
 from tqdm import tqdm
-from src.models import *
+from src.models import lstm, cnn, mlp, cldnn, tcn
 from src.utils import auxiliary_plots, metrics
 from src.preprocessing import normalization, data_generation
 
@@ -23,9 +23,35 @@ PAST_HISTORY = [24, 48, 72, 96, 120, 144, 168]
 BATCH_SIZE = [16, 36, 64, 128]
 BUFFER_SIZE = 10000
 
-EPOCHS = [25, 50, 100, 200]
+EPOCHS = [1, 25, 50, 100, 200]
 
-results = []
+METRICS = ['mse', 'rmse', 'nrmse', 'mae', 'mpe', 'mape', 'mdape', 'smape', 'smdape',
+           'mase', 'rmspe', 'rmsse', 'mre', 'rae', 'mrae', 'std_ae', 'std_ape']
+
+TCN_PARAMS = {
+    'nb_filters': [32, 64, 128],
+    'kernel_size': [2, 3, 6],
+    'nb_stacks': [1, 2, 3],
+    'dilations': [[1, 2, 4, 8], [1, 2, 4, 8, 16], [1, 2, 4, 8, 16, 32]],
+    'use_skip_connections': [False, True],
+    'dropout_rate': [0, 0.15],
+    'use_batch_norm': [False, True],
+}
+LSTM_PARAMS = {
+    'num_stack_layers': [1, 2, 3, 4],
+    'units': [32, 64, 128],
+    'dropout': [0, 0.15]
+}
+
+# Write result csv header
+with open(RESULT_FILE_NAME, 'w') as resfile:
+    resfile.write(';'.join([str(a) for a in
+                            ['MODEL', 'MODEL_DESCRIPTION', 'FORECAST_HORIZON', 'PAST_HISTORY', 'BATCH_SIZE', 'EPOCHS'] +
+                            METRICS +
+                            ['val_'+m for m in METRICS] +
+                            ['loss', 'val_loss']]
+                           )
+                  + "\n")
 
 # Read train file
 with open(TRAIN_FILE_NAME, 'r') as datafile:
@@ -40,7 +66,7 @@ with open(TEST_FILE_NAME, 'r') as datafile:
     ts_test = np.reshape(ts_test, (ts_test.shape[0],))
 
 # Train/validation split
-TRAIN_SPLIT = int(ts_train.shape[0]*0.8)
+TRAIN_SPLIT = int(ts_train.shape[0] * 0.8)
 
 # Normalize training data
 norm_params = normalization.get_normalization_params(ts_train[:TRAIN_SPLIT])
@@ -51,35 +77,37 @@ ts_test = normalization.normalize(ts_test, norm_params)
 for past_history, batch_size, epochs in tqdm(list(itertools.product(PAST_HISTORY, BATCH_SIZE, EPOCHS))):
     # Get x and y for training and validation
     x_train, y_train = data_generation.univariate_data(ts_train, 0, TRAIN_SPLIT, past_history, FORECAST_HORIZON)
-    x_val, y_val = data_generation.univariate_data(ts_train, TRAIN_SPLIT-past_history, ts_train.shape[0], past_history, FORECAST_HORIZON)
+    x_val, y_val = data_generation.univariate_data(ts_train, TRAIN_SPLIT - past_history, ts_train.shape[0],
+                                                   past_history, FORECAST_HORIZON)
     # Get x and y for test data
     x_test, y_test = data_generation.univariate_data(ts_test, 0, ts_test.shape[0], past_history, FORECAST_HORIZON)
 
     # Convert numpy data to tensorflow dataset
-    train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train)).cache().shuffle(BUFFER_SIZE).batch(batch_size).repeat()
+    train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train)).cache().shuffle(BUFFER_SIZE).batch(
+        batch_size).repeat()
     val_data = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size).repeat()
     test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
 
-
     # Create model
-    model_list = {
-        'lstm': simple_lstm(x_train.shape, FORECAST_HORIZON, 'adam', 'mae'),
-        'mlp': mlp(x_train.shape, FORECAST_HORIZON, 'adam', 'mae', hidden_layers=[64,32,16]),
-        'cldnn': cldnn(x_train.shape, FORECAST_HORIZON, 'adam', 'mae'),
-        'cnn': cnn(x_train.shape, FORECAST_HORIZON, 'adam', 'mae'),
-        # 'tcn': tcn(x_train.shape, FORECAST_HORIZON, 'adam', 'mae'),
-    }
+    model_list = {**{'TCN_{}'.format(j): (tcn, [x_train.shape, FORECAST_HORIZON, 'adam', 'mae', *params]) for j, params in enumerate(itertools.product(*TCN_PARAMS.values()))},
+                  **{'LSTM_{}'.format(j): (lstm, [x_train.shape, FORECAST_HORIZON, 'adam', 'mae', *params]) for j, params in enumerate(itertools.product(*LSTM_PARAMS.values()))}}
+
+    model_description_list = {**{'TCN_{}'.format(j): str(dict(zip(TCN_PARAMS.keys(), params))) for j, params in
+                                 enumerate(itertools.product(*TCN_PARAMS.values()))},
+                              **{'LSTM_{}'.format(j): str(dict(zip(LSTM_PARAMS.keys(), params))) for j, params in
+                                 enumerate(itertools.product(*LSTM_PARAMS.values()))}}
 
     evaluation_interval = int(np.ceil(x_train.shape[0] / batch_size))
 
-    for model_name, model in model_list.items():
+    for model_name, (model_function, params) in model_list.items():
+        model = model_function(*params)
         print(model.summary())
 
         # Train the model
         # TODO: Callbacks
         history = model.fit(train_data, epochs=epochs,
-                              steps_per_epoch=evaluation_interval,
-                              validation_data=val_data, validation_steps=evaluation_interval)
+                            steps_per_epoch=evaluation_interval,
+                            validation_data=val_data, validation_steps=evaluation_interval)
 
         # Plot training and evaluation loss evolution
         if SHOW_PLOTS:
@@ -90,7 +118,7 @@ for past_history, batch_size, epochs in tqdm(list(itertools.product(PAST_HISTORY
         val_forecast = normalization.denormalize(val_forecast, norm_params)
         y_val = normalization.denormalize(y_val, norm_params)
 
-        val_metrics = metrics.evaluate_all(y_val, val_forecast)
+        val_metrics = metrics.evaluate(y_val, val_forecast, METRICS)
         print('Validation metrics', val_metrics)
 
         # TEST
@@ -101,7 +129,7 @@ for past_history, batch_size, epochs in tqdm(list(itertools.product(PAST_HISTORY
         y_test = normalization.denormalize(y_test, norm_params)
         x_test = normalization.denormalize(x_test, norm_params)
 
-        test_metrics = metrics.evaluate_all(y_test, test_forecast)
+        test_metrics = metrics.evaluate(y_test, test_forecast, METRICS)
         print('Test scores', test_metrics)
 
         # Plot some test predictions
@@ -109,8 +137,9 @@ for past_history, batch_size, epochs in tqdm(list(itertools.product(PAST_HISTORY
             auxiliary_plots.plot_ts_forecasts(x_test, y_test, test_forecast)
 
         # Save results
-        val_metrics = {'val_'+k: val_metrics[k] for k in val_metrics}
+        val_metrics = {'val_' + k: val_metrics[k] for k in val_metrics}
         model_metric = {'MODEL': model_name,
+                        'MODEL_DESCRIPTION': model_description_list[model_name],
                         'FORECAST_HORIZON': FORECAST_HORIZON,
                         'PAST_HISTORY': past_history,
                         'BATCH_SIZE': batch_size,
@@ -119,9 +148,8 @@ for past_history, batch_size, epochs in tqdm(list(itertools.product(PAST_HISTORY
                         **val_metrics,
                         **history.history
                         }
-        results.append(model_metric)
 
-
-results = pd.DataFrame(results).to_csv(RESULT_FILE_NAME, index=False)
+        with open(RESULT_FILE_NAME, 'a') as resfile:
+            resfile.write(';'.join([str(a) for a in model_metric.values()]) + "\n")
 
 
--- src/models.py
+++ src/models.py
@@ -1,25 +1,64 @@
 import tensorflow as tf
 from tcn import TCN
-from keras import  layers, Model
 
 
-def simple_lstm(input_shape, output_size=1, optimizer='adam', loss='mae'):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(50, input_shape=input_shape[-2:]),
-        tf.keras.layers.Dense(output_size)
-    ])
+def lstm(input_shape, output_size=1, optimizer='adam', loss='mae', num_stack_layers=1, units=50, dropout=0, hidden_dense_layers=[]):
+    inputs = tf.keras.layers.Input(shape=input_shape[-2:])
+    return_sequences = num_stack_layers>1
+    x = tf.keras.layers.LSTM(units, return_sequences=return_sequences, dropout=dropout)(inputs)
+    for i in range(num_stack_layers-1):
+        return_sequences = i<num_stack_layers-2
+        x = tf.keras.layers.LSTM(units, return_sequences=return_sequences, dropout=dropout)(x)
+    for hidden_units in hidden_dense_layers:
+        x = tf.keras.layers.Dense(hidden_units)(x)
+    x = tf.keras.layers.Dense(output_size)(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=x)
     model.compile(optimizer=optimizer, loss=loss)
 
     return model
 
 
-def mlp(input_shape, output_size=1, optimizer='adam', loss='mae', hidden_layers=[32,16,8]):
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Input(shape=input_shape[-2:]))
-    model.add(tf.keras.layers.Flatten())  # Convert the 2d input in a 1d array
+def gru(input_shape, output_size=1, optimizer='adam', loss='mae', hidden_recurrent_layers=[50], hidden_dense_layers=[],
+        return_sequences=True):
+    inputs = tf.keras.layers.Input(shape=input_shape[-2:])
+    x = tf.keras.layers.LSTM(hidden_recurrent_layers.pop(0))(inputs)
+    for hidden_units in hidden_recurrent_layers:
+        x = tf.keras.layers.GRU(hidden_units, return_sequences=return_sequences)(x)
+    for hidden_units in hidden_dense_layers:
+        x = tf.keras.layers.Dense(hidden_units)(x)
+    x = tf.keras.layers.Dense(output_size)(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=x)
+    model.compile(optimizer=optimizer, loss=loss)
+
+    return model
+
+
+def ernn(input_shape, output_size=1, optimizer='adam', loss='mae', hidden_recurrent_layers=[50], hidden_dense_layers=[],
+         return_sequences=True):
+    inputs = tf.keras.layers.Input(shape=input_shape[-2:])
+    x = tf.keras.layers.LSTM(hidden_recurrent_layers.pop(0))(inputs)
+    for hidden_units in hidden_recurrent_layers:
+        x = tf.keras.layers.SimpleRNN(hidden_units, return_sequences=return_sequences)(x)
+    for hidden_units in hidden_dense_layers:
+        x = tf.keras.layers.Dense(hidden_units)(x)
+    x = tf.keras.layers.Dense(output_size)(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=x)
+    model.compile(optimizer=optimizer, loss=loss)
+
+    return model
+
+
+def mlp(input_shape, output_size=1, optimizer='adam', loss='mae', hidden_layers=[32, 16, 8]):
+    inputs = tf.keras.layers.Input(shape=input_shape[-2:])
+    x = tf.keras.layers.Flatten()(inputs)  # Convert the 2d input in a 1d array
     for hidden_units in hidden_layers:
-        model.add(tf.keras.layers.Dense(hidden_units))
-    model.add(tf.keras.layers.Dense(output_size))
+        x = tf.keras.layers.Dense(hidden_units)(x)
+    x = tf.keras.layers.Dense(output_size)(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=x)
     model.compile(optimizer=optimizer, loss=loss)
 
     return model
@@ -40,18 +79,20 @@ def cnn(input_shape, output_size=1, optimizer='adam', loss='mae'):
     return model
 
 
-def tcn(input_shape, output_size=1, optimizer='adam', loss='mae'):
-    # TODO fix: 'RepeatDataset' object has no attribute 'ndim'
-    inputs = layers.Input(shape=input_shape[-2:])
-    x = TCN()(inputs)
-    #x = layers.Flatten()(x)
-    x = layers.Dense(1024)(x)
-    outputs = layers.Dense(output_size)(x)
+def tcn(input_shape, output_size=1, optimizer='adam', loss='mae', nb_filters=64, kernel_size=2, nb_stacks=1,
+        dilations=[1, 2, 4, 8, 16, 32], use_skip_connections=True, dropout_rate=0, use_batch_norm=False, activation='linear',):
+    inputs = tf.keras.layers.Input(shape=input_shape[-2:])
 
-    model = Model(inputs=inputs, outputs=outputs)
+    x = TCN(nb_filters=nb_filters, kernel_size=kernel_size, nb_stacks=nb_stacks, dilations=dilations,
+            use_skip_connections=use_skip_connections, dropout_rate=dropout_rate, activation=activation,
+            use_batch_norm=use_batch_norm)(inputs)
+    outputs = tf.keras.layers.Dense(output_size)(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer=optimizer, loss=loss)
 
     return model
+
 
 def cldnn(input_shape, output_size=1, optimizer='adam', loss='mae'):
     inputs = tf.keras.layers.Input(shape=input_shape[-2:])
